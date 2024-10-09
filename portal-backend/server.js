4715be8dc2829e10d93a1c054b7c2de6f7dc39f2
Rishi Rao <22raor@gmail.com>
2024-10-09 01:20:20 -0400

    5000 bad

--- portal-backend/server.js
+++ portal-backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/applications', applicationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
