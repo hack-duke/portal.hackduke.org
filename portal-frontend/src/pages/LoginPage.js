c708cd24e1e73c1c8403cc3976dbd034afe318e9
Kevin <kevinmao660@gmail.com>
2024-11-09 21:44:07 -0500

    initial login page styling

--- portal-frontend/src/pages/LoginPage.js
+++ portal-frontend/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate } from 'react-router-dom';
+import { Navbar } from '../components/navbar';
 
 const LoginPage = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -10,7 +11,14 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
+    <div style = {{
+      backgroundImage: `url(/images/Login.png)`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh',
+      width: '100vw',
+    }}>
+      <Navbar />
       <h2>Login</h2>
       <button onClick={() => loginWithRedirect()}>Log In</button>
     </div>
