59f960d6d70e75c8929f3aed24edce656056c425
Kevin <kevinmao660@gmail.com>
2024-11-10 15:59:15 -0500

    Made login button and fixed background.

--- portal-frontend/src/pages/LoginPage.js
+++ portal-frontend/src/pages/LoginPage.js
@@ -2,25 +2,67 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate } from 'react-router-dom';
 import { Navbar } from '../components/navbar';
+import { useEffect } from 'react';
 
 const LoginPage = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+  }, []);
 
   if (isAuthenticated) {
     return <Navigate to="/application" replace />;
   }
 
   return (
+    
     <div style = {{
-      backgroundImage: `url(/images/Login.png)`,
+      position: 'relative',
+      backgroundImage: `url(/images/BG_Elements.svg)`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       height: '100vh',
       width: '100vw',
+      left: '-10px',
     }}>
       <Navbar />
-      <h2>Login</h2>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
+
+      <div> 
+        
+      </div>
+      <h2 style = {{
+        position: 'relative',
+        background: '#0042C6',
+        font: 'urbanist',
+        width: '10.2vw',                     // Set responsive width based on viewport width
+        height: '3.75vw',                  // Set responsive height based on viewport width to maintain aspect ratio
+        // marginTop: '59.5vh',                 // Adjust based on the layout
+        // marginLeft: '62vw',
+      }}>Login</h2>
+
+      <button
+        style={{
+          backgroundImage: `url('/images/Log_In_Button.png')`,
+          backgroundSize: 'cover',           // Ensures the image covers the button's area
+          backgroundPosition: 'center',      // Centers the image within the button
+          backgroundRepeat: 'no-repeat',     // Prevents the image from tiling
+          width: '10.2vw',                     // Set responsive width based on viewport width
+          height: '3.75vw',                  // Set responsive height based on viewport width to maintain aspect ratio
+          marginTop: '59.5vh',                 // Adjust based on the layout
+          marginLeft: '62vw',
+          border: 'none',
+          cursor: 'pointer',
+          color: 'transparent',
+          backgroundColor: 'transparent',    // Ensures no extra background color
+          position: 'absolute',              // Allows precise placement with margin values
+        }}
+        onClick={() => loginWithRedirect()}
+      >
+        Log In
+      </button>
+
+
     </div>
   );
 };
