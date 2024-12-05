91e09a5e251c5b31399b1d5a49242fc2cf7ab23a
rally.lin@duke.edu <linrally@gmail.com>
2024-12-05 13:57:14 -0500

    remove mui

--- portal-frontend/src/App.js
+++ portal-frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ApplicationPage from './pages/ApplicationPage';
@@ -9,7 +8,6 @@ import AdminPage from './pages/AdminPage'
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  const theme = createTheme();
   const { isLoading } = useAuth0();
 
   if (isLoading) {
@@ -17,23 +15,22 @@ function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route 
-          path="/application" 
-          element={
-            <ProtectedRoute>
-              <ApplicationPage />
-            </ProtectedRoute>
-          } 
-          />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>);
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route 
+        path="/application" 
+        element={
+          <ProtectedRoute>
+            <ApplicationPage />
+          </ProtectedRoute>
+        } 
+        />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
