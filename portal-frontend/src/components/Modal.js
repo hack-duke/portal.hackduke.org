534b801c0b3bf4440b145f72ab7f190624e8b29e
rally.lin@duke.edu <linrally@gmail.com>
2024-12-23 13:26:54 +0900

    add error modal

---
+++ portal-frontend/src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Modal.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export const ModalHeader = ({ children }) => {
+    return <h1 className="modal-header">{children}</h1>;
+};
+
+export default Modal;
