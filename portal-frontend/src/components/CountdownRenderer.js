00af70bb48a6d0e83cb174950a55b372f9601638
Arvindh Manian <arvindh.manian@gmail.com>
2025-01-09 20:25:05 -0500

    Better countdown text

---
+++ portal-frontend/src/components/CountdownRenderer.js
@@ -0,0 +1,9 @@
+const CountdownRenderer = ({days, hours, minutes, seconds}) => {
+    return (
+        <div className="hero-countdown">
+        <p>Due in {days} days {hours} hours {minutes} minutes {seconds} seconds</p>
+        </div>
+    )
+}
+
+export default CountdownRenderer
