7e0dc840d483f5924cb6a33f3ea00fa6a0913acb
Sarah Tran <smt105@duke.edu>
2025-09-21 22:17:29 +0000

    undo move models

--- portal-backend-python/models/form.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from models.base import Base
-
-
-class Form(Base):
-    __tablename__ = "form"
-
-    form_key = Column(String, primary_key=True)
-    year = Column(Integer, nullable=False)
