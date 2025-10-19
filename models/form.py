2652dc91eccd86259bf4cf8f15363e9a8ba57e9a
Amelia Lane <amelialane427@gmail.com>
2025-10-19 20:23:13 +0000

    upgraded form to include is_open field

--- models/form.py
+++ models/form.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from models.base import Base
+import sqlalchemy as sa
 
 
 class Form(Base):
     __tablename__ = "form"
-
     form_key = Column(String, primary_key=True)
     year = Column(Integer, nullable=False)
+    is_open = Column(Boolean, nullable=False, default=False, server_default=sa.text("false"))
