d425b42f42b0dec57405aebb23ca5214465b2690
Rishi Rao <22raor@gmail.com>
2025-11-17 23:24:45 -0500

    didn't mean to remove import oops

--- portal-frontend/src/pages/FormPage.js
+++ portal-frontend/src/pages/FormPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { MultiPageForm } from "../components/form/Form";
