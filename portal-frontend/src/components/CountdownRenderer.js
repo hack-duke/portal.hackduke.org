47733c6211d3c80fc5ae876fa2f3fd590f0b2811
Arvindh Manian <arvindh.manian@gmail.com>
2025-12-13 01:03:41 +0000

    add countdowns for 2026 cycle

--- portal-frontend/src/components/CountdownRenderer.js
+++ portal-frontend/src/components/CountdownRenderer.js
@@ -1,11 +1,24 @@
-const CountdownRenderer = ({ days, hours, minutes, seconds }) => {
+import PropTypes from "prop-types";
+import React from "react";
+
+const CountdownRenderer = ({ days, hours, minutes, seconds, deadlineType }) => {
   return (
     <div className="hero-countdown">
       <p>
-        Due in {days} days {hours} hours {minutes} minutes {seconds} seconds
+        {deadlineType} due in {days} days {hours} hours {minutes} minutes{" "}
+        {seconds} seconds
       </p>
     </div>
   );
+};
+
+// this part was required by the linter?
+CountdownRenderer.propTypes = {
+  days: PropTypes.number.isRequired,
+  hours: PropTypes.number.isRequired,
+  minutes: PropTypes.number.isRequired,
+  seconds: PropTypes.number.isRequired,
+  deadlineType: PropTypes.string.isRequired,
 };
 
 export default CountdownRenderer;
